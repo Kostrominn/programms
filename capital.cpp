4f102dbd5dc1e55e9dbd80c0fb1c256200884cf0
Kostrominn <52157970+Kostrominn@users.noreply.github.com>
2019-06-25 18:06:24 +0300

    Update capital.cpp

--- capital.cpp
+++ capital.cpp
@@ -6,52 +6,57 @@ using namespace std;
 void Change_capital(map<string, string>& a, string tmp){
 	string country, new_capital;
 	cin >> country >> new_capital;
-	for (auto i : a){
-		if (*i.first == country && new_capital == *i.second){
+	map<string, string> :: iterator i;
+	for (i = a.begin(); i != a.end(); i++){
+		if ((*i).first == country && new_capital == (*i).second){
 			cout << "Country" << country << "hasn't changed its capital" << endl;
 			break;
 		}
-		if( *i.first == country && new_capital != *i.second){
-			string old_capital = i.second;
-			*i.second = new_capital;
+		if( (*i).first == country && new_capital != (*i).second){
+			string old_capital = (*i).second;
+			(*i).second = new_capital;
 			cout << "Country" << country << "has changed its capital from" << old_capital << "to" << new_capital << endl;
 			break;
 		}
 	}
 	cout << "Introduce new country" << country << "with capital" << new_capital << endl; 
-	a.insert(country, new_capital;)
+	a.insert(country, new_capital);
 }
 void Rename (map<string, string>& a){
 	string old_country, new_country;
 	cin >> old_country >> new_country;
-	for (auto i : a){
-		if (*i.first == new_country){
-			if (old_country == new_coutry){
-				cout << Incorrect rename, skip << endl;
+	map<string, string> :: iterator i;
+	for (i = a.begin(); i != a.end(); i++){
+		if ((*i).first == new_country){
+			if (old_country == new_country){
+				cout << "Incorrect rename, skip" << endl;
 			}
 			else{
-				*i.first = new_country;
-				cout << "Country" << old_country << "with capital" << capital << "has been renamed to" << new_country << endl;
+				(*i).first = new_country;
+				cout << "Country" << old_country << "with capital" << (*i).second << "has been renamed to" << new_country << endl;
 			}
 		}
 	}
 }
 void About (map<string, string>& a){
+	string country;
 	cin >> country;
-	for (auto i : a){
-		if (*i.first == country){
-			cout << "Country" << country << "has capital" << map[i].second << endl;
+	map<string, string> :: iterator i;
+	for (i = a.begin(); i != a.end(); i++){
+		if ((*i).first == country){
+			cout << "Country" << country << "has capital" << (*i).second << endl;
 		}
 	}
 }
 
 void Dump (map<string, string>& a){
+	map<string, string> :: iterator i;
 	if (a.size() == 0){
 		cout << "There are no countries in the world" << endl;
 	}
 	else{
-		for (auto i : m){
-			cout << *i.first <<  "/" << *i.second << endl;
+		for (i = a.begin(); i != a.end(); i++){
+			cout << (*i).first <<  "/" << (*i).second << endl;
 		}
 	}
 }
@@ -60,16 +65,16 @@ int main(){
 	map<string, string> a;
 	string tmp;
 	cin >> tmp;
-	if ( tmp == CHANGE_CAPITAL){
-		Change_capital (tmp);
+	if ( tmp == "CHANGE_CAPITAL"){
+		Change_capital (a, tmp);
 	}
-	if ( tmp == RENAME){
-		Rename (tmp);
+	if ( tmp == "RENAME"){
+		Rename (a);
 	}
-	if ( tmp == ABOUT){
-		About (tmp);
+	if ( tmp == "ABOUT"){
+		About (a);
 	}
-	if ( tmp == DUMP){
-		Dump (tmp);
+	if ( tmp == "DUMP"){
+		Dump (a);
 	}
 }
